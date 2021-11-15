3e2dfa0e9b6d9446bd91351dcca9da026becd76c
thinhdaotrong <thinh.daotrong178@gmail.com>
2021-11-15 16:35:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -72,8 +72,8 @@ function App() {
 
   const estimatePose = (positions) => {
     const numRows = 4;
-    // const imagePoints = cv.matFromArray(numRows, 2, cv.CV_64FC1, getPoints4(positions));
-    const imagePoints = cv.matFromArray(numRows, 2, cv.CV_64FC1);
+    const imagePoints = cv.matFromArray(numRows, 2, cv.CV_64FC1, getPoints4(positions));
+    // const imagePoints = cv.matFromArray(numRows, 2, cv.CV_64FC1);
     const modelPoints = cv.matFromArray(numRows, 3, cv.CV_64FC1, [
       0,
       0,
@@ -108,18 +108,18 @@ function App() {
       1,
     ]);
 
-    [
-      positions[30].x,
-      positions[30].y, // nose tip
-      positions[30].x,
-      positions[30].y, // nose tip
-      positions[36].x,
-      positions[36].y, // left corner of left eye
-      positions[45].x,
-      positions[45].y, // right corner of right eye
-    ].map((v, i) => {
-      imagePoints.data64F[i] = v;
-    });
+    // [
+    //   positions[30].x,
+    //   positions[30].y, // nose tip
+    //   positions[30].x,
+    //   positions[30].y, // nose tip
+    //   positions[36].x,
+    //   positions[36].y, // left corner of left eye
+    //   positions[45].x,
+    //   positions[45].y, // right corner of right eye
+    // ].map((v, i) => {
+    //   imagePoints.data64F[i] = v;
+    // });
 
     const distCoeffs = cv.Mat.zeros(4, 1, cv.CV_64FC1);
     const rotationVector = new cv.Mat({ width: 1, height: 3 }, cv.CV_64FC1); // cv.Mat.zeros(1, 3, cv.CV_64FC1);
@@ -165,7 +165,7 @@ function App() {
     );
 
     let rotationVectorDegree = rotationVector.data64F.map((d) => (d / Math.PI) * 180);
-    // console.log('rotationVectorDegree: ', rotationVectorDegree[0]);
+    console.log('rotationVectorDegree: ', rotationVectorDegree[0]);
 
     imagePoints.delete();
     modelPoints.delete();
@@ -177,6 +177,92 @@ function App() {
     return rotationVectorDegree;
   };
 
+  const estimatePose2 = (positions) => {
+    const numRows = 6;
+    const imagePoints = cv.matFromArray(numRows, 2, cv.CV_64FC1, getPoints4(positions));
+    const modelPoints = cv.matFromArray(numRows, 3, cv.CV_64FC1, [
+      0,
+      0,
+      0, // Nose tip
+      0,
+      0,
+      0, // Nose tip
+      // 0, -330, -65, // Chin
+      -225,
+      170,
+      -135, // Left eye left corner
+      225,
+      170,
+      -135, // Right eye right corne
+      // -150, -150, -125,  // Left Mouth corner
+      // 150, -150, -125,  // Right mouth corner
+    ]);
+
+    const size = { width: 640, height: 480 };
+    const focalLength = size.width;
+    const center = [size.width / 2, size.height / 2];
+
+    const cameraMatrix = cv.matFromArray(3, 3, cv.CV_64FC1, [
+      focalLength,
+      0,
+      center[0],
+      0,
+      focalLength,
+      center[1],
+      0,
+      0,
+      1,
+    ]);
+
+    const distCoeffs = cv.Mat.zeros(4, 1, cv.CV_64FC1);
+    const rotationVector = cv.Mat.zeros(1, 3, cv.CV_64FC1);
+    const translationVector = cv.Mat.zeros(1, 3, cv.CV_64FC1);
+    cv.solvePnP(modelPoints, imagePoints, cameraMatrix, distCoeffs, rotationVector, translationVector);
+    const rotationVectorMatrix = cv.Mat.zeros(3, 3, cv.CV_64FC1);
+    cv.Rodrigues(rotationVector, rotationVectorMatrix);
+    const matVector = new cv.MatVector();
+    matVector.push_back(rotationVectorMatrix);
+    matVector.push_back(translationVector);
+    const projectMatrix = cv.Mat.zeros(3, 4, cv.CV_64FC1);
+    cv.hconcat(matVector, projectMatrix);
+    const noArray0 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+    const noArray1 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+    const noArray2 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+    const noArray3 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+    const noArray4 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+    const noArray5 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+    const eulerAngles = cv.Mat.zeros(1, 3, cv.CV_64FC1);
+    cv.decomposeProjectionMatrix(
+      projectMatrix,
+      noArray0,
+      noArray1,
+      noArray2,
+      noArray3,
+      noArray4,
+      noArray5,
+      eulerAngles
+    );
+    const [yaw, pitch] = eulerAngles.data64F.map((degree) => Math.asin(Math.sin((degree / 180) * Math.PI)));
+    console.log('yaw, pitch: ', yaw, pitch);
+    imagePoints.delete();
+    modelPoints.delete();
+    cameraMatrix.delete();
+    distCoeffs.delete();
+    rotationVector.delete();
+    translationVector.delete();
+    rotationVectorMatrix.delete();
+    projectMatrix.delete();
+    matVector.delete();
+    noArray0.delete();
+    noArray1.delete();
+    noArray2.delete();
+    noArray3.delete();
+    noArray4.delete();
+    noArray5.delete();
+    eulerAngles.delete();
+    return { pitch, yaw };
+  };
+
   const onPlay = async () => {
     const result = await faceapi
       .detectSingleFace(videoRef.current, new faceapi.TinyFaceDetectorOptions())
@@ -184,7 +270,7 @@ function App() {
       .withFaceExpressions();
 
     if (result) {
-      const rotationVectorDegree = estimatePose(result.landmarks.positions);
+      const rotationVectorDegree = estimatePose2(result.landmarks.positions);
     }
 
     setTimeout(() => onPlay(), 100);
