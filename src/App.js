1a4f08a8ee4ddfe4c5e0ce397809b420f10b46b1
thinhdaotrong <thinh.daotrong178@gmail.com>
2021-11-16 12:02:25 +0700

    update

--- src/App.js
+++ src/App.js
@@ -92,6 +92,7 @@ function App() {
       // -150.0, -150.0, -125.0,  // Left Mouth corner
       // 150.0, -150.0, -125.0,  // Right mouth corner
     ]);
+    console.log('modelPoints: ', modelPoints);
 
     // Camera internals
     const size = { width: 640, height: 480 };
@@ -108,22 +109,14 @@ function App() {
       0,
       1,
     ]);
+    console.log('cameraMatrix: ', cameraMatrix);
 
     const imagePoints = cv.Mat.zeros(numRows, 2, cv.CV_64FC1);
     const distCoeffs = cv.Mat.zeros(4, 1, cv.CV_64FC1); // Assuming no lens distortion
     const rvec = new cv.Mat({ width: 1, height: 3 }, cv.CV_64FC1);
     const tvec = new cv.Mat({ width: 1, height: 3 }, cv.CV_64FC1);
 
-    const pointZ = cv.matFromArray(1, 3, cv.CV_64FC1, [0.0, 0.0, 500.0]);
-    const pointY = cv.matFromArray(1, 3, cv.CV_64FC1, [0.0, 500.0, 0.0]);
-    const pointX = cv.matFromArray(1, 3, cv.CV_64FC1, [500.0, 0.0, 0.0]);
-    const noseEndPoint2DZ = new cv.Mat();
-    const nose_end_point2DY = new cv.Mat();
-    const nose_end_point2DX = new cv.Mat();
-    const jaco = new cv.Mat();
-
     const ns = positions[30];
-    console.log('ns: ', ns);
     const le = positions[36];
     const re = positions[45];
 
@@ -158,55 +151,11 @@ function App() {
       rvec.data64F[2] = -3.0;
     }
 
-    console.log(
-      'truoc',
-      rvec.data64F.map((d) => (d / Math.PI) * 180)
-    );
-
     const success = cv.solvePnP(modelPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec, true);
     if (!success) return;
 
     let rvecDegree = rvec.data64F.map((d) => (d / Math.PI) * 180);
     console.log('rvecDegree: ', rvecDegree);
-
-    // cv.projectPoints(pointZ, rvec, tvec, cameraMatrix, distCoeffs, noseEndPoint2DZ, jaco);
-    // cv.projectPoints(pointY, rvec, tvec, cameraMatrix, distCoeffs, nose_end_point2DY, jaco);
-    // cv.projectPoints(pointX, rvec, tvec, cameraMatrix, distCoeffs, nose_end_point2DX, jaco);
-
-    let im = cv.imread(canvasRef.current);
-    for (var i = 0; i < numRows; i++) {
-      cv.circle(
-        im,
-        {
-          x: imagePoints.doublePtr(i, 0)[0],
-          y: imagePoints.doublePtr(i, 1)[0],
-        },
-        3,
-        [255, 0, 255, 255],
-        -1
-      );
-    }
-    // draw axis
-    const pNose = { x: imagePoints.data64F[0], y: imagePoints.data64F[1] };
-    const pZ = {
-      x: noseEndPoint2DZ.data64F[0],
-      y: noseEndPoint2DZ.data64F[1],
-    };
-    const p3 = {
-      x: nose_end_point2DY.data64F[0],
-      y: nose_end_point2DY.data64F[1],
-    };
-    const p4 = {
-      x: nose_end_point2DX.data64F[0],
-      y: nose_end_point2DX.data64F[1],
-    };
-    cv.line(im, pNose, pZ, [255, 0, 0, 255], 2);
-    cv.line(im, pNose, p3, [0, 255, 0, 255], 2);
-    cv.line(im, pNose, p4, [0, 0, 255, 255], 2);
-
-    // Display image
-    cv.imshow(canvasRef.current, im);
-    im.delete();
 
     imagePoints.delete();
     modelPoints.delete();
@@ -214,13 +163,6 @@ function App() {
     distCoeffs.delete();
     rvec.delete();
     tvec.delete();
-    pointZ.delete();
-    pointY.delete();
-    pointX.delete();
-    noseEndPoint2DZ.delete();
-    nose_end_point2DY.delete();
-    nose_end_point2DX.delete();
-    jaco.delete();
 
     return rvecDegree;
   };
@@ -228,6 +170,7 @@ function App() {
   const estimatePose2 = async (positions) => {
     const cv = await opencv;
     const numRows = 6;
+
     const imagePoints = cv.matFromArray(numRows, 2, cv.CV_64FC1, getPoints6(positions));
     const modelPoints = cv.matFromArray(numRows, 3, cv.CV_64FC1, [
       0,
@@ -280,6 +223,7 @@ function App() {
     const noArray0 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
     const noArray1 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
     const noArray2 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
+
     const noArray3 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
     const noArray4 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
     const noArray5 = cv.Mat.zeros(0, 0, cv.CV_64FC1);
@@ -309,6 +253,7 @@ function App() {
     noArray1.delete();
     noArray2.delete();
     noArray3.delete();
+
     noArray4.delete();
     noArray5.delete();
     eulerAngles.delete();
@@ -398,11 +343,6 @@ function App() {
     //   rvec.data64F[2] = -3.0;
     // }
 
-    console.log(
-      'truoc',
-      rvec.data64F.map((d) => (d / Math.PI) * 180)
-    );
-
     const success = cv.solvePnP(modelPoints, imagePoints, cameraMatrix, distCoeffs, rvec, tvec, true);
     if (!success) return;
 
@@ -427,6 +367,7 @@ function App() {
   const onPlay = async () => {
     const result = await faceapi
       .detectSingleFace(videoRef.current, new faceapi.TinyFaceDetectorOptions())
+
       .withFaceLandmarks();
 
     if (result) {
